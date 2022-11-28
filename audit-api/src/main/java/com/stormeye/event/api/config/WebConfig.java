1a73e8076bd838068cc8171c6c74bd5a3f5a76d7
cnorburn <carl.norburn@gmail.com>
2022-11-28 10:58:12 +0000

    audit api - add front end config yaml to the ingress for https forwarding add certs to ingress

--- audit-api/src/main/java/com/stormeye/event/api/config/WebConfig.java
+++ audit-api/src/main/java/com/stormeye/event/api/config/WebConfig.java
@@ -1,8 +1,17 @@
 package com.stormeye.event.api.config;
 
+import static java.util.Collections.singletonList;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import java.util.List;
 import javax.annotation.PostConstruct;
 
 /**
@@ -21,4 +30,19 @@ public class WebConfig {
     public void configureDispatcherServlet() {
         this.dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
     }
+
+    @Bean
+    @ConditionalOnProperty(value = "management.endpoints.web.cors.allowed-origins")
+    public CorsConfigurationSource corsConfigurationSource(
+            @Value("${management.endpoints.web.cors.allowed-origins}") final List<String> corsOrigins) {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(corsOrigins);
+        configuration.setAllowedMethods(singletonList("GET"));
+        configuration.setAllowedHeaders(singletonList("*"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }
